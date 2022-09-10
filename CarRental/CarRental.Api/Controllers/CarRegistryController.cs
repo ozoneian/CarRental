21296f713a55d7c5a23c1412f41f53be8670f344
ozoneian <71430787+ozoneian@users.noreply.github.com>
2022-09-10 11:53:11 +0200

    Add solution and api

---
+++ CarRental/CarRental.Api/Controllers/CarRegistryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarRegistryController : ControllerBase
+    {
+        [HttpPost("RegisterDelivery")]
+        public void DeliverCar()
+        {
+           
+        }
+
+        [HttpPost("RegisterReturn")]
+        public decimal ReturnCarAndGetPayment()
+        {
+            return 0;
+        }
+    }
+}
